48e1dd12de39df87195583a1cc8feca70c4041d5
Rafael Acioly <aciolyr@gmail.com>
2016-09-08 21:37:42 +0000

    initial project

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
 # Multiplo upload com CI v3.+
 
-[![Code Climate](https://codeclimate.com/github/rafa-acioly/multiple-upload-CI/badges/gpa.svg)](https://codeclimate.com/github/rafa-acioly/multiple-upload-CI)
-
 ## Como usar
 
-1. Copie o arquivo controller para sua pasta de controllers.
-2. Inclua a função `hasFile` na classe `Upload` - [linha 1324](https://github.com/rafa-acioly/multiple-upload-CI/blob/master/system/libraries/Upload.php#L1324) - *você pode também copiar esta função a incluir diretamente no controller para evitar mecher no core do framework*
-3. Defina a chave que ira usar em `$_FILES`
-4. Defina as configurações de validação pra os arquivos enviados - *veja todas as configurações na [documentação do codeigniter](https://www.codeigniter.com/user_guide/libraries/file_uploading.html#preferences)*
+1. Copie ou mova o arquivo MY_Upload para a pasta controllers `(application/controllers)`
+2. Inclua a classe em seu controller
+```php
+require_once (dirname(__FILE__) . '/MY_Upload.php');
 
-Crie um formulario direcionando os dados para a classe `upload` usando a função `config_upload`
-
-```PHP
-<?php form_open_multipart('upload/do_upload'); ?>
-  <input type="file" name="userFile[]" multiple />
-  <input type="submit" value="Enviar" />
-<?php form_close(); ?>
+class YourClasse extends CI_Controller
+{
+  ...
 ```
+3. Defina a chave usada em `$_FILES`
+```php
+$upload = new MY_Upload('yourKey');
+$upload->start(); // Inicia o upload
+$upload->fileNames; // Array com os nomes dos arquivos
+```
+
+## Default
+
+- Arquivos permitidos gif, png e jpg
+- Nomes de arquivos criptografados
+
+## Opcional
+
+- Todos os nomes dos arquivos salvos serão armazenados em `$upload->fileNames`
+- Altere configurações com `$upload->configuration['upload_path] = 'my_path_to_folter'`
+  - upload_path   (string)
+  - allowed_types (string: 'gif|jpg|jpeg...')
+  - encrypt_name  (true ou false)
 
 ## Como salvar no BD?
 
--- Upload.php
+++ Upload.php
@@ -1,147 +1,127 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Upload extends CI_Controller
+class MY_Upload
 {
-	/**
-	 * 
-	 * Storage the encrypt name of files 
-	 * @type array
-	 */
-	public $fileNames = [];
-	
-	
-	/**
-	 * 
-	 * Define the messages for sessions
-	 * @type array
-	 */
-	public $alert = [
-			'success' => 'All file uploaded successfully',
-			'error' => 'Sorry something gone wrong with upload, please contact the admin',
-			'fatal' => 'Could not upload the files',
-		];
-	
-	
-	/**
-	 * 
-	 * Set the configuration for uploaded files
-	 * @type array
-	 */ 
-	public $configuration = [
-			'upload_path' => 'uploads/file/',
-			'allowed_types' => 'gif|jpg|png',
-			'encrypt_name' => TRUE
-		];
-	
-	
-	/**
-	 * 
-	 * Load the main modules to use form and url on the view
-	 * 
-	 */ 
-	public function  __construct() 
-	{
-		parent::__construct();
-		$this->load->model('arquivo_model');
-		$this->load->library(['upload']);
-		$this->load->helper(['form', 'url']);
-	}
-	
-	/**
-	 * 
-	 * Call the main view
-	 * 
-	 */
-	public function index()
-	{
+    /**
+     * Storage the encrypt name of files.
+     *
+     * @var array
+     */
+    public $fileNames = [];
 
-		$this->load->view('index');
-	}
-	
-	
-	/**
-	 * 
-	 * Perform loop to upload files if any key is defined
-	 * 
-	 */
-	public function do_upload()
-	{
-		if (!$this->validateFiles('userFile')) {
-			throw new Exception($this->alert['fatal'].' '.$this->upload->display_errors(), 1);
-		}
-		
-		/**
-		 * Set your model and function to save the file's name in database
-		 */
-		if (!$this->YOUR_MODEL->YOUR_FUNCTION($this->fileNames)) {
-			$this->session->set_flashdata('statusMsg', $this->alert["error"]);
-			$this->load->view('index');
-		}
-		
-		$this->session->set_flashdata('statusMsg', $this->alert["success"]);
-		$this->load->view('index');
-	}
-	
-	
-	/**
-	 * 
-	 * Validate files given on the key
-	 * @param string $key
-	 * @return bool
-	 */ 
-	public function validateFiles($key)
-	{
-		if ($this->hasFile($key)) {
-			
-			$filesCount = count($_FILES[$key]['name']);
-			$files = $_FILES[$key];
-			
-			for($i = 0; $i < $filesCount; $i++){
-				$_FILES[$key]['name'] = $files['name'][$i];
-				$_FILES[$key]['type'] = $files['type'][$i];
-				$_FILES[$key]['tmp_name'] = $files['tmp_name'][$i];
-				$_FILES[$key]['error'] = $files['error'][$i];
-				$_FILES[$key]['size'] = $files['size'][$i];
+    /**
+     * Use the key used in global variable $_FILES
+     *
+     * @var string
+     */
+    public $key;
+    
+    /*
+     * Instance of core for libraries
+     * 
+     */
+    protected $ci;
+    
+    /**
+     * Store the error message if has any
+     *
+     * @var string
+     */
+    public $messageError;
 
-				//TODO: Verificar nesta etapa se todos os arquivos enviados estao sendo validados pela função do_upload.
-				if (!$this->saveFiles($key)) {
-					// salva um registro no arquivo de log com o erro.
-					log_message($this->upload->display_errors().": ".date('d/m/Y H:m:s'));
-					return FALSE;
-				}
-			}
-		}
-		return TRUE;
-	}
-	
-	/**
-	 * 
-	 * Check if there is a key defined on $_FILES
-	 * @param string $key
-	 * @return bool
-	 */ 
-	public function hasFile($key)
-	{
-		return isset($_FILES[$key]);
-	}
-	
-	
-	/**
-	* Save the files on folder 
-	* push a new key with the encrypted file's name to array
-	*
-	* @return bool
-	*/
-	public function saveFiles()
-	{
-		$this->upload->initialize($this->configuration);
-		
-		if (!$this->upload->do_upload($key)) {
-			return FALSE;
-		}
-		
-		// Save file's name in array
-		array_push($this->fileNames, $this->upload->data('file_name')); 
-		return TRUE;
-	}
+    /**
+     * Set the configuration for uploaded files.
+     *
+     * @var array
+     */
+    public $configuration = [
+            'upload_path' => 'uploads/files/',
+            'allowed_types' => 'gif|jpg|png',
+            'encrypt_name' => true,
+        ];
+
+    /**
+     * Load the main modules to use form and url on the view.
+     */
+    public function __construct($key)
+    {
+        $this->key = $key;
+        $this->ci =& get_instance();
+    }
+    
+    /**
+     * Perform loop to upload files if any key is defined.
+     */
+    public function start()
+    {
+        if (!$this->hasFile()) {
+            throw new Exception("The key {$this->key} does not exist", 1);
+        }
+        
+        if (!$this->validateFiles()) {
+            throw new Exception($this->messageError, 1);
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if there is a key defined on $_FILES.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function hasFile()
+    {
+        return isset($_FILES[$this->key]);
+    }
+
+    /**
+     * Save the files on folder
+     * push a new key with the encrypted file's name to array.
+     *
+     * @return bool
+     */
+    public function saveFiles()
+    {
+        $this->ci->upload->initialize($this->configuration);
+
+        if (!$this->ci->upload->do_upload($this->key)) {
+            return false;
+        }
+
+        // Save file's name in array
+        array_push($this->fileNames, $this->ci->upload->data('file_name'));
+
+        return true;
+    }
+
+    /**
+     * Validate files given on the key.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function validateFiles()
+    {
+        $filesCount = count($_FILES[$this->key]['name']);
+        $files = $_FILES[$this->key];
+
+        for ($i = 0; $i < $filesCount; ++$i) {
+            $_FILES[$this->key]['name'] = $files['name'][$i];
+            $_FILES[$this->key]['type'] = $files['type'][$i];
+            $_FILES[$this->key]['tmp_name'] = $files['tmp_name'][$i];
+            $_FILES[$this->key]['error'] = $files['error'][$i];
+            $_FILES[$this->key]['size'] = $files['size'][$i];
+            
+            if (!$this->saveFiles($this->key)) {
+                $this->messageError = $this->ci->upload->display_errors();
+                return false;
+            }
+        }
+        return true;
+    }
 }
